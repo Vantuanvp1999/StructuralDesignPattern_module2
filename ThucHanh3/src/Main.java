9e7602a46e4c685caaba25f6a5187987e633fd6c
Tuân <nguyenvantuanvp1999@gmail.com>
2025-05-08 09:40:45 +0700

    add

---
+++ ThucHanh3/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter file or folder path: ");
+        String path = scanner.nextLine();
+        FileCalculator fileCalculator = new FileCalculatorAdapter();
+        Client client = new Client(fileCalculator);
+        client.printFileSize(path);
+    }
+}
